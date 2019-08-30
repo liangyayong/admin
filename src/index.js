8dacd742419a2590377e050c6027d7fad6c15067
皇者归来liangyayong <1185558645@qq.com>
2019-08-30 21:39:07 +0800

    recat第一次

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const tit = <p>我是p标签</p>
+// const list = [{
+//     id:1,name:'jock'
+// },
+// {
+//     id:2,name:'tom'
+// },{
+//     id:3,name:'jm'
+// },{
+//     id:4,name:'js'
+// }]
+// const title = (<ul className='ul'>{
+//    list.map(item=><li key={item.id}>{item.name}</li>)
+// }
+    
+// </ul>)
+// - 需求：实现评论列表功能
+// - 1 如果有评论数据，就展示列表结构 li（ 列表渲染 ）
+//   - name 表示评论人，渲染 h3
+//   - content 表示评论内容，渲染 p
+// - 2 如果没有评论数据，就展示一个 h1 标签，内容为： 暂无评论！
+// - 3 根据自己的喜好添加样式
+const list = [
+    { id: 1, name: 'jack', content: 'rose, you jump i jump' },
+    { id: 2, name: 'rose', content: 'jack, you see you, one day day' },
+    { id: 3, name: 'tom', content: 'jack,。。。。。' },
+    { id: 4, name: 'tim', content: 'jack,。。升水。。。' },
+    { id: 5, name: 't11', content: 'jack,。。问问。。。' },
+  ]
+//   第一种方法
+  const fn= ()=>{
+      if (list.length>0) {
+        return   <ul className='ul'>{
+              list.map(item=><li key={item.id}> <h3>评论人:{item.name}</h3>
+               <p>评论内容:{item.content}</p>
+              </li>)
+          }</ul>   
+      }else{
+        return <h1> 暂无评论</h1>
+      }
+  }
+  const title = <div className='app'>{fn()}</div>
+// const li = (<ul className='ul'>{
+//     list.map(item=><li key={item.id}>
+//         <h3> 评论人:{item.name}</h3>
+//         <p> 评论内容：{item.content}</p>
+//     </li>)
+// }</ul>)
+// const li2 =(
+//     <h1>站务评论</h1>
+// )
+// const isshow = list.length>0
+//   const title = <div className='app'>{isshow? li: li2}</div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(title,document.getElementById('root'))
